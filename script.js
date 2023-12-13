79bee611979c0c27927f06344e41852becf60e54
ernestwkumm <139768776+ernestwkumm@users.noreply.github.com>
2023-12-13 13:34:10 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,6 @@
+var hamburger = document.querySelector(".hamburger-menu");
+var menu = document.querySelector(".menu");
 
-const btn = document.querySelector(".hamburger-menu");
-const logo = document.querySelector(".Logo_full");
-const paragraph = document.querySelector(".paragraph")
-
-
-
-loadData()
-
-async function loadData(){
-  const res = await fetch("https://wekazxfljduiwarktyca.supabase.co/rest/v1/inventoryData", {
-    headers:{
-      apikey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indla2F6eGZsamR1aXdhcmt0eWNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk1ODM0MzYsImV4cCI6MjAwNTE1OTQzNn0.F-BMBoJT6hIMygLOP3r_-5Uvm_AUQV0dg6E3BDYOqAQ",
-      authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indla2F6eGZsamR1aXdhcmt0eWNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk1ODM0MzYsImV4cCI6MjAwNTE1OTQzNn0.F-BMBoJT6hIMygLOP3r_-5Uvm_AUQV0dg6E3BDYOqAQ"
-    }
-  });
-  
-  const data = await res.json();
-  console.log(data);
-  const [id,created,code] = data;
-  console.log(id);
-
-}
-
-
-paragraph.innerHTML = id;
-
-
-
-// btn.addEventListener("click", function() {
-
-//   if (logo.classList.contains("hidden")) {
-//     logo.classList.remove("hidden");
-
-//   } else{
-//     logo.classList.add("hidden");
-//   }
-// } )
+hamburger.addEventListener("click", function () {
+  menu.classList.toggle("showmenu");
+});
