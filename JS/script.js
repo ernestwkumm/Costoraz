cac472286c51b33daf315ed5cfc8d44a1a802f29
ernestwkumm <139768776+ernestwkumm@users.noreply.github.com>
2024-01-12 17:00:48 +0200

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+var hamburger = document.querySelector(".hamburger-menu");
+var menu = document.querySelector(".menu");
+
+// hamburger.addEventListener("click", function () {
+//   menu.classList.toggle("showmenu");
+// });
